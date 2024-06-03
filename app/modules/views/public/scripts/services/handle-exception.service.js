b710784dfc3529cd4f3a5a4e85c19f942067d7c3
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2024-06-02 23:34:10 -0300

    :sparkles: feat: melhoria do roteamento, ajuste na arquitetura e conexao entre as telas

---
+++ app/modules/views/public/scripts/services/handle-exception.service.js
@@ -0,0 +1,14 @@
+import { toast } from "../utils/toast.util.js";
+
+export function handleException(error) {
+	const http_codes = {
+	400: () => 'info',
+	401: () => 'info',
+	404: () => 'info',
+	409: () => 'info',
+	500: () => 'error',
+	}
+	toast(error.data.message, http_codes[error.status]);
+}
+
+
