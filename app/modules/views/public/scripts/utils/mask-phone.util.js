9485a5efb96d07c93c4a38139df9ce21e58f4e89
Paulo Almeida <paulo333henrique00@gmail.com>
2024-10-20 20:23:41 -0300

    Delete app/modules directory

--- app/modules/views/public/scripts/utils/mask-phone.util.js
+++
@@ -1,9 +0,0 @@
-export function maskPhone(value) {
-	return value.replace(/[^\d]/g, "").replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
-
-}
-
-export function clearMask(value) {
-	return value.replace(/[.\-/]/g, "");
-}
-
