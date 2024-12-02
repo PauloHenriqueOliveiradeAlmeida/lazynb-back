b95625e93fe2a65799e9fbd4993cb708f74975d1
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-12-01 23:27:33 -0300

    :sparkles: feat: cep endpoint

--- app/Api/Modules/Property/PropertyController.php
+++ app/Api/Modules/Property/PropertyController.php
@@ -5,6 +5,7 @@ namespace App\Api\Modules\Property;
 use App\Api\Modules\Property\PropertyService;
 use App\Api\Shared\Guards\Enums\UserLevelEnum;
 use App\Api\Shared\Guards\UserGuard;
+use App\Api\Shared\Services\Cep\Gateways\ViacepGateway;
 use Raven\Falcon\Attributes\Controller;
 use Raven\Falcon\Attributes\HttpMethods\Delete;
 use Raven\Falcon\Attributes\HttpMethods\Get;
@@ -18,10 +19,11 @@ use Raven\Falcon\Attributes\Request\Param;
 class PropertyController
 {
 	public function __construct(
-		private readonly PropertyService $propertyService = new PropertyService()
+		private readonly PropertyService $propertyService = new PropertyService(new ViacepGateway)
 	) {}
 
 	#[Post]
+	#[UseGuard(new UserGuard(UserLevelEnum::ADMIN))]
 	public function create(#[Body] PropertyDto $propertyDto)
 	{
 		return $this->propertyService->create($propertyDto);
@@ -42,6 +44,7 @@ class PropertyController
 	}
 
 	#[Get(endpoint: ':id')]
+	#[UseGuard(new UserGuard(UserLevelEnum::ALL))]
 	public function get(#[Param(paramName: 'id')] int $id)
 	{
 		return $this->propertyService->getById($id);
@@ -52,5 +55,12 @@ class PropertyController
 	public function delete(#[Param(paramName: 'id')] int $id)
 	{
 		return $this->propertyService->delete($id);
+	}
+
+	#[Get(endpoint: ':cep/address')]
+	#[UseGuard(new UserGuard(UserLevelEnum::ADMIN))]
+	public function getAddressByCep(#[Param(paramName: 'cep')] string $cep)
+	{
+		return $this->propertyService->getAddressByCep($cep);
 	}
 }
